847b5de3516bd5a1c523b6744c61e5f61f24c040
Rahul-Tawar <rahultawar2004@gmail.com>
2024-12-24 15:57:21 +0530

    Add preload

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-
 import Home from './pages/home/Home'
 import About from './pages/about/About'
 import './index.css'
 import CourseDetails from './pages/course-details/CourseDetails'
 
-
-
-
 function App() {
 
+  const [ loading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false)
+    }, 3000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/courses/:courseName' element={<CourseDetails/>}/>
-      </Routes>
-    </BrowserRouter>
+    <>
+    { loading ? (
+      <div>
+        Loading...
+      </div>
+    ): (
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/courses/:courseName' element={<CourseDetails/>}/>
+        </Routes>
+      </BrowserRouter>
+    )}
+  </>
+  
   )
+  
 }
 
 export default App
+
+
